cb192054a6f3488e1b0a2fcb6bef9cb806e3ee15
Andreas Prucha (Abexto/Helicon) <andreas.prucha@gmail.com>
2018-04-18 22:24:31 +0200

    Interm. commit

---
+++ src/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Copyright 2018 Andreas Prucha, Abexto - Helicon Software Development.
+ */
+
+namespace abexto\amylian\yii\doctrine\orm;
+
+/**
+ * Description of Configuration
+ *
+ * @author Andreas Prucha, Abexto - Helicon Software Development
+ */
+class Configuration extends \abexto\amylian\yii\doctrine\dbal\Configuration
+{
+    //put your code here
+}
